2dd26fd57f8d051304b67963b11f27ffa03aa6ae
MarceloBorges <melombm@hotmail.com>
2020-08-28 18:31:24 -0500

    somar todos os valores do array

---
+++ bloco_4/exercises/4_2/exercises_2.js
@@ -0,0 +1,12 @@
+// criando um array
+let numbers = [2,5,3 ];
+
+//criando uma variavel para guardar valor
+let sumAll =0;
+
+// percorrendo um array e imprimindo o valores
+for(let cont = 0; cont < numbers.length; cont++ ){
+    sumAll += numbers[cont];
+  
+}
+console.log(sumAll);
