dac9c3a57a738913a587a448b1238e35d8e51c62
MarceloBorges <melombm@hotmail.com>
2020-08-31 14:04:50 -0500

    add comentario

--- bloco_4/exercises/4_2/exercises_6.js
+++ bloco_4/exercises/4_2/exercises_6.js
@@ -1,14 +1,16 @@
 // criando um array
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+// criando uma variavel para armazenar
 let result = 0;
 
+//função para achar ser o array é impar
 for (let i = 0; i < numbers.length; i++) {
   if (numbers[i] % 2 !== 0) {
     result += 1;
   }
 }
-
+//função se for par
 if (result === 0) {
   console.log('nenhum valor ímpar encontrado');
 } else {
