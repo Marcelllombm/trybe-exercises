d7bf76af875412c2985662972243cdbee13993fe
MarceloBorges <melombm@hotmail.com>
2020-08-31 14:20:18 -0500

    criando um array que va 1 a 25

---
+++ bloco_4/exercises/4_2/exercises_8.js
@@ -0,0 +1,10 @@
+// criando um array para armazenar
+let numbers = [];
+
+// criando um for de 1 a 25
+for (let i = 0; i < 25; i += 1) {
+    numbers.push(i);
+  }
+
+// mostrar no console
+  console.log(numbers);
