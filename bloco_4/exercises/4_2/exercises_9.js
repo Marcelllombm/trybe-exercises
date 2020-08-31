05a03269881293264ebbd59ce9fc92651c13baad
MarceloBorges <melombm@hotmail.com>
2020-08-31 14:24:24 -0500

    dividir os valores do arrays por 2

---
+++ bloco_4/exercises/4_2/exercises_9.js
@@ -0,0 +1,7 @@
+// criando um array
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// dividir por 2 os valor da array
+for (let i = 0; i < numbers.length; i += 1) {
+    console.log(numbers[i] / 2);
+  };
