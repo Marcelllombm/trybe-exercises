39407fef95eeb24f7e33fe5c84d6d8271cffad69
MarceloBorges <melombm@hotmail.com>
2020-10-09 23:21:07 -0500

    ex1 foreach

---
+++ bloco_8/class/ex1-foreach.js
@@ -0,0 +1,30 @@
+// criar um função que perminte enviar um email para cada pessoal cadastrada no array usado foreach
+
+let listOfPeople = [
+    'ada.lovelaca@examplo.com',
+    'marie.curi@examplo.com',
+    'margaret.hamilt@example.com',
+    'alan.turi@example.com'
+];
+const sendEmail = (email) => {
+  console.log(`Email para ${email} foi enviado com sucesso`);
+};
+console.log("\nModelo usado for\n");
+// modelo  for
+for (index = 0; index < listOfPeople.length; index += 1) {
+    let emailItems= [];
+    emailItems += listOfPeople[index];
+    console.log(emailItems); 
+    // console.log(`sua posição é: ${index}`);
+  }
+console.log("\nModelo usado forEach\n");
+// foreEach e obrigatoramente ter um paramentro e tem dois opcionais.
+listOfPeople.forEach((item, position, array)=> {
+  sendEmail(item);
+// no  segundo paramentro mostra a posição "position" do array
+console.log(`sua posição é: ${position}`);
+// percorre todas as posição do array ou mostra quantidade do array
+console.log(`A quantidade das pessoas do processo é: ${array.length}`);
+});
+
+
